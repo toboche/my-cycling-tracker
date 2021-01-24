27affb5091ad8b5ae7a962eb231cefbbc63e6ce6
toboche <toboche@gmail.com>
2021-01-24 08:21:24 +0100

    Basic presenter implementation

---
+++ app/src/test/java/pl/toboche/mycyclingtracker/base/PresenterTest.kt
@@ -0,0 +1,30 @@
+package pl.toboche.mycyclingtracker.base
+
+import com.nhaarman.mockito_kotlin.mock
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class PresenterTest {
+    class TestPresenter<View> : Presenter<View>()
+
+    val mockView = mock<Any>()
+    val systemUnderTest = TestPresenter<Any>()
+
+    @Test
+    fun `should attach view`() {
+        systemUnderTest.attach(mockView)
+
+        assertThat(systemUnderTest.view)
+            .isEqualTo(mockView)
+    }
+
+    @Test
+    fun `should detach view`() {
+        systemUnderTest.attach(mockView)
+        systemUnderTest.detach()
+
+        assertThat(systemUnderTest.view)
+            .isNull()
+    }
+
+}
